a13c95c8ba20521d899f89c2263be2202a6cc0b1
expert <expert-way@hotmail.com>
2020-08-15 10:19:20 +0300

    first init

---
+++ App.js
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React,{Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+class App extends Component {
+
+  render (){
+    return  (
+        <>
+          <View/>
+        </>
+    );
+  }
+
+}
+
+export default App;
